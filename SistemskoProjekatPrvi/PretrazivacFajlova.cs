9f979a2d4c5aacdf85ebeb28f1b92205f90fa5c8
Dimitrije <dimitrije.ilic2004@gmail.com>
2025-09-07 17:51:52 +0200

    Add project files.

---
+++ SistemskoProjekatPrvi/PretrazivacFajlova.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemskoProjekatPrvi
+{
+    public class PretrazivacFajlova
+    {
+        static readonly object workerLocker = new();
+        static int brojAktivnihNiti = 0;
+        static readonly StringBuilder ret = new("");
+        static int brojFajlova = 0;
+        public static String PretraziSaNitima(String kljuc)
+        {
+            brojAktivnihNiti = 0;
+            brojFajlova = 0;
+            ret.Clear();
+            String content = "";
+
+            try
+            {
+                String path = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\.."));
+
+                String[] fajlovi = Directory.GetFiles(path);
+
+                foreach (String f in fajlovi)
+                {
+                    String imeFajla = Path.GetFileName(f);
+                    if (imeFajla.ToLower().Contains(kljuc))
+                    {
+                        brojFajlova++;
+                        ret.Append($"<p><a href=\"/download?putanja={Uri.EscapeDataString(f)}\"> {imeFajla} </a></p>");
+                    }
+                }
+
+                String[] direktorijumi = Directory.GetDirectories(path);
+
+                brojAktivnihNiti += direktorijumi.Length;
+
+                foreach (String dir in direktorijumi)
+                {
+                    ThreadPool.QueueUserWorkItem(PretraziDirSaNitima, new Tuple<String, String>(dir, kljuc));
+                }
+
+                lock (workerLocker)
+                {
+                    while (brojAktivnihNiti > 0)
+                    {
+                        Monitor.Wait(workerLocker);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Javio se izuzetak: " + e.Message);
+            }
+
+            Console.WriteLine("Sa nitima: Zavrsena pretraga!");
+
+            if (String.IsNullOrEmpty(ret.ToString()))
+            {
+                content += "<html> <head>\n";
+                content += "<title>Projekat 1 - Rezultat</title>\n";
+                content += "</head>\n";
+                content += "<body>\n";
+                content += "<h1>Sistemsko programiranje - Projekat 1</h1>\n";
+                content += $"<h3>Rec koju ste pretrazili je: <span style='color:blue;'>{kljuc}</span></h3>\n";
+                content += "<h3>Nije pronadjen nijedan fajl na osnovu kljuca.</h3>\n";
+                content += "</body> </html>\n";
+
+                return content;
+            }
+
+            content += "<html> <head>\n";
+            content += "<title>Projekat 1 - Rezultat</title>\n";
+            content += "</head>\n";
+            content += "<body>\n";
+            content += "<h1>Sistemsko programiranje - Projekat 1</h1>\n";
+            content += $"<h3>Rec koju ste pretrazili je: <span style='color:blue;'>{kljuc}</span></h3>\n";
+            content += $"<h3>Rezultati pretrage: </h3>\n";
+            content += $"<h4>Broj pronadjenih fajlova: {brojFajlova} </h4>\n";
+            content += "<h4>Kliknite na fajl da biste ga preuzeli.</h4>\n";
+            content += $"<h4>{ret}</h4>\n";
+            content += "</body> </html>\n";
+
+            return content;
+        }
+
+        static void PretraziDirSaNitima(object? state)
+        {
+            try
+            {
+                var (path, kljuc) = ((Tuple<String, String>)state!);
+
+                String files = "";
+                int brFiles = 0;
+
+                String[] fajlovi = Directory.GetFiles(path);
+
+                foreach (String f in fajlovi)
+                {
+                    String imeFajla = Path.GetFileName(f);
+                    if (imeFajla.ToLower().Contains(kljuc))
+                    { 
+                        brFiles++;
+                        files += $"<p><a href=\"/download?putanja={Uri.EscapeDataString(f)}\"> {imeFajla} </a></p>";
+                    }
+                }
+
+                String[] direktorijumi = Directory.GetDirectories(path);
+
+                if (direktorijumi.Length > 0)
+                {
+                    lock (workerLocker)
+                    {
+                        brojAktivnihNiti += direktorijumi.Length;
+                    }
+                }
+
+                foreach (String dir in direktorijumi)
+                {
+                    ThreadPool.QueueUserWorkItem(PretraziDirSaNitima, new Tuple<String, String>(dir, kljuc));
+                }
+
+                lock (workerLocker)
+                {
+                    ret.Append(files);
+                    brojFajlova += brFiles;
+                    brojAktivnihNiti--;
+
+                    if (brojAktivnihNiti == 0)
+                        Monitor.Pulse(workerLocker);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Javio se izuzetak tokom izvrsenja niti: " + e.Message);
+
+                lock (workerLocker)
+                {
+                    brojAktivnihNiti--;
+
+                    if (brojAktivnihNiti == 0)
+                        Monitor.Pulse(workerLocker);
+                }
+            }
+        }
+
+        public static String PretraziBezNiti(String kljuc)
+        {
+            ret.Clear();
+            brojFajlova = 0;
+            String content = "";
+
+            try
+            {
+                String path = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\.."));
+
+                String[] fajlovi = Directory.GetFiles(path);
+
+                foreach (String f in fajlovi)
+                {
+                    String imeFajla = Path.GetFileName(f);
+                    if (imeFajla.ToLower().Contains(kljuc))
+                    {
+                        brojFajlova++;
+                        ret.Append($"<p><a href=\"/download?putanja={Uri.EscapeDataString(f)}\"> {imeFajla} </a></p>");
+                    }
+                }
+
+                String[] direktorijumi = Directory.GetDirectories(path);
+
+                foreach (String dir in direktorijumi)
+                {
+                    ret.Append(PretraziDirBezNiti(dir, kljuc));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Bez niti: Zavrsena pretraga!");
+
+            if (String.IsNullOrEmpty(ret.ToString()))
+            {
+                content += "<html> <head>\n";
+                content += "<title>Projekat 1 - Rezultat</title>\n";
+                content += "</head>\n";
+                content += "<body>\n";
+                content += "<h1>Sistemsko programiranje - Projekat 1</h1>\n";
+                content += $"<h3>Rec koju ste pretrazili je: <span style='color:blue;'>{kljuc}</span></h3>\n";
+                content += "<h3>Nije pronadjen nijedan fajl na osnovu kljuca.</h3>\n";
+                content += "</body> </html>\n";
+
+                return content;
+            }
+
+            content += "<html> <head>\n";
+            content += "<title>Projekat 1 - Rezultat</title>\n";
+            content += "</head>\n";
+            content += "<body>\n";
+            content += "<h1>Sistemsko programiranje - Projekat 1</h1>\n";
+            content += $"<h3>Rec koju ste pretrazili je: <span style='color:blue;'>{kljuc}</span></h3>\n";
+            content += $"<h3>Rezultati pretrage: </h3>\n";
+            content += $"<h4>Broj pronadjenih fajlova: {brojFajlova} </h4>\n";
+            content += "<h4>Kliknite na fajl da biste ga preuzeli.</h4>\n";
+            content += $"<h4>{ret}</h4>\n";
+            content += "</body> </html>\n";
+
+            return content;
+        }
+
+        static String PretraziDirBezNiti(String path, String kljuc)
+        {
+            String files = "";
+
+            try
+            {
+                String[] fajlovi = Directory.GetFiles(path);
+
+                foreach (String f in fajlovi)
+                {
+                    String imeFajla = Path.GetFileName(f);
+                    if (imeFajla.ToLower().Contains(kljuc))
+                    {
+                        brojFajlova++;
+                        files += $"<p><a href=\"/download?putanja={Uri.EscapeDataString(f)}\"> {imeFajla} </a></p>";
+                    }
+                }
+
+                String[] direktorijumi = Directory.GetDirectories(path);
+
+                foreach (String dir in direktorijumi)
+                {
+                    files += PretraziDirBezNiti(dir, kljuc);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Javio se izuzetak tokom pretrage u {path}: {e.Message}");
+            }
+
+            return files;
+        }
+    }
+}
---
+++ SistemskoProjekatPrvi/Program.cs
@@ -0,0 +1,245 @@
+﻿using SistemskoProjekatPrvi;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SistemskoProjekatPrvi
+{
+    internal class Program
+    {
+        private static readonly int port = 5000;
+        private static readonly KesLRU m_kes = new(3);
+        private static readonly Stopwatch m_stopwatch = new();
+
+        static void Main(string[] args)
+        {
+            static TimeSpan zaustaviStopwatch()
+            {
+                m_stopwatch.Stop();
+                var vreme = m_stopwatch.Elapsed;
+                m_stopwatch.Reset();
+                return vreme;
+            }
+
+            try
+            {
+                TcpListener listener = new TcpListener(IPAddress.Any, port);
+                listener.Start();
+                Console.WriteLine($"Otvoren server na portu {port}");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                    StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+                    StreamWriter writer = new StreamWriter(stream, Encoding.ASCII) { AutoFlush = true };
+
+                    string? request = reader.ReadLine();
+                    if (string.IsNullOrEmpty(request))
+                        continue;
+
+                    Console.WriteLine("------------------------------------------------------------");
+                    Console.WriteLine($"Stigao je zahtev {request}.");
+
+                    string[] tokeni = request.Split(' ');
+                    if (tokeni.Length <= 2)
+                        continue;
+
+                    string method = tokeni[0];
+                    string sadrzaj = tokeni[1];
+
+                    if (method == "GET")
+                    {
+                        int pos1 = sadrzaj.IndexOf('/');
+
+                        String metoda = "";
+                        int index = pos1+1;
+                        while (index < sadrzaj.Length)
+                        {
+                            if (sadrzaj[index] == ' ' || sadrzaj[index] == '?')
+                                break;
+                            metoda += sadrzaj[index];
+                            index++;
+                        }
+
+                        Console.WriteLine("Metoda je " + metoda);
+
+                        String response = "";
+                        if (metoda == "pronadji-fajlove")
+                        {
+                            try
+                            {
+                                int start = sadrzaj.IndexOf("kljuc=");
+
+                                if (start == -1)
+                                {
+                                    Console.WriteLine("Primljen nevalidan zahtev!");
+
+                                    String html = "";
+                                    html += "<html> <head>\n";
+                                    html += "<title>Projekat 1 - Rezultat</title>\n";
+                                    html += "</head>\n";
+                                    html += "<body>\n";
+                                    html += "<h1> Nevalidan zahtev!</h1>\n";
+                                    html += "</body> </html>\n";
+
+                                    response += "HTTP/1.1 200 OK\r\n";
+                                    response += "Content-Type: text/html; charset=UTF-8\r\n";
+                                    response += $"Content-Length: " + html.Length + "\r\n\r\n";
+                                    response += html;
+
+                                    writer.Write(response);
+                                    client.Close();
+                                    continue;
+                                }
+
+                                String kljuc = sadrzaj.Substring(start + "kljuc=".Length);
+
+                                Console.WriteLine("Primljen zahtev za pretragu sa kljucem " + kljuc);
+
+                                String ret;
+                                m_stopwatch.Start();
+
+                                if (m_kes.Citaj(kljuc, out String? value))
+                                {
+                                    var vreme = zaustaviStopwatch();
+                                    Console.WriteLine($"Podatak je pronadjen u kesu za {vreme}s");
+                                    ret = value;
+                                }
+                                else
+                                {
+                                    zaustaviStopwatch();
+                                    Console.WriteLine("Nema odgovarajucih podataka u kesu, krece pretraga!");
+
+                                    m_stopwatch.Start();
+                                    ret = PretrazivacFajlova.PretraziBezNiti(kljuc);
+                                    var vreme_bez_niti = zaustaviStopwatch();
+
+                                    m_stopwatch.Start();
+                                    ret = PretrazivacFajlova.PretraziSaNitima(kljuc);
+                                    var vreme_sa_nitima = zaustaviStopwatch();
+                                    
+                                    Console.WriteLine($"Vreme bez niti: {vreme_bez_niti}s");
+                                    Console.WriteLine($"Vreme sa nitima: {vreme_sa_nitima}s");
+
+                                    m_kes.Upisi(kljuc, ret);
+                                }
+
+                                response = "";
+                                response += "HTTP/1.1 200 OK\r\n";
+                                response += "Content-Type: text/html; charset=UTF-8\r\n";
+                                response += $"Content-Length: " + ret.Length + "\r\n\r\n";
+                                response += ret;
+
+                                writer.Write(response);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Greska tokom pretrage fajlova: " + e.Message);
+                            }
+                        }
+                        else if (metoda == "download")
+                        {
+                            try
+                            {
+                                int start = sadrzaj.IndexOf("putanja=");
+
+                                if (start == -1)
+                                {
+                                    Console.WriteLine("Primljen nevalidan zahtev!");
+
+                                    String html = "";
+                                    html += "<html> <head>\n";
+                                    html += "<title>Projekat 1 - Rezultat</title>\n";
+                                    html += "</head>\n";
+                                    html += "<body>\n";
+                                    html += "<h1> Nevalidan zahtev!</h1>\n";
+                                    html += "</body> </html>\n";
+
+                                    response += "HTTP/1.1 200 OK\r\n";
+                                    response += "Content-Type: text/html; charset=UTF-8\r\n";
+                                    response += $"Content-Length: " + html.Length + "\r\n\r\n";
+                                    response += html;
+
+                                    writer.Write(response);
+                                    client.Close();
+                                    continue;
+                                }
+
+                                String putanja = Uri.UnescapeDataString(sadrzaj.Substring(start + "putanja=".Length));
+
+                                if (!System.IO.File.Exists(putanja))
+                                {
+                                    throw new IOException("Putanja nije pronadjena.");
+                                }
+
+                                String imeFajla = Path.GetFileName(putanja);
+
+                                Console.WriteLine("Preuzet fajl: " + imeFajla);
+
+                                var podaci = System.IO.File.ReadAllBytes(putanja);
+                                string header = "";
+                                header += "HTTP/1.1 200 OK\r\n";
+                                header += "Content-Type: application/octet-stream\r\n";
+                                header += $"Content-Disposition: attachment; filename=\"{imeFajla}\"\r\n";
+                                header += $"Content-Length: {podaci.Length}\r\n\r\n";
+
+                                byte[] headerBytes = Encoding.ASCII.GetBytes(header);
+                                stream.Write(headerBytes, 0, headerBytes.Length);
+                                stream.Write(podaci, 0, podaci.Length);
+                            }
+                            catch (IOException e)
+                            {
+                                Console.WriteLine("Greska tokom citanja fajla: " + e.Message);
+                                string notFound = "";
+                                notFound += "HTTP/1.1 404 Not Found\r\n";
+                                notFound += "Content-Length: 0\r\n\r\n";
+
+                                byte[] notFoundBytes = Encoding.ASCII.GetBytes(notFound);
+                                stream.Write(notFoundBytes, 0, notFoundBytes.Length);
+
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            String html = "";
+                            html += "<html> <head>\n";
+                            html += "<title>Projekat 1</title>\n";
+                            html += "</head>\n";
+                            html += "<body>\n";
+                            html += "<h1>Sistemsko programiranje - Projekat 1</h1>\n";
+                            html += "<form action=\"/pronadji-fajlove\" method=GET>\n";
+                            html += "<p> Pretrazite fajlove servera: <input name=\"kljuc\">\n";
+                            html += "<input type=submit value=\"Pretrazi\">\n";
+                            html += "</form>\n";
+                            html += "</body> </html>\n";
+
+                            response += "HTTP/1.1 200 OK\r\n";
+                            response += "Content-Type: text/html; charset=UTF-8\r\n";
+                            response += $"Content-Length: " + html.Length + "\r\n\r\n";
+                            response += html;
+
+                            writer.Write(response);
+                        }
+                    }
+                    
+                    client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Javio se izuzetak: " + e.Message);
+            }
+        }
+    }
+}
